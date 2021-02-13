a23f94bb74256bf606cdcfc4dd5f0895b5f6c548
davidot <david.tuin@gmail.com>
2021-02-13 18:03:16 +0100

    Split project and add dependency
    We now have:
     - main lib
     - executable for main lib
     - test of main lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "Action.h"
 
 int main() {
+    Action a {};
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
---
+++ test/ActionTest.cpp
@@ -0,0 +1,6 @@
+#include <catch2/catch.hpp>
+#include <Action.h>
+
+TEST_CASE("BasicActionTest") {
+    Action a{};
+}
