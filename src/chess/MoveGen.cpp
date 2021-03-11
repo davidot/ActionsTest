ce4ac4b987b4008444ddc8744a1bbc307f2c0903
davidot <david.tuin@gmail.com>
2021-03-11 12:45:51 +0100

    Add more tests

--- src/chess/MoveGen.cpp
+++ src/chess/MoveGen.cpp
@@ -19,8 +19,23 @@ namespace Chess {
         for (BI col = 0; col < board.size(); col++) {
             for (BI row = 0; row < board.size(); row++) {
                 auto opt_piece = board.pieceAt(col, row);
-                if (opt_piece && opt_piece->isPawn()) {
-                    add_move(col, row, col, row + 1);
+                if (!opt_piece) {
+                    continue;
+                }
+                switch (opt_piece->type()) {
+                    case Piece::Type::Pawn:
+                        add_move(col, row, col, row + 1);
+                        break;
+                    case Piece::Type::King:
+                        break;
+                    case Piece::Type::Knight:
+                        break;
+                    case Piece::Type::Bishop:
+                        break;
+                    case Piece::Type::Rook:
+                        break;
+                    case Piece::Type::Queen:
+                        break;
                 }
             }
         }
--- test/chess/MoveGen.cpp
+++ test/chess/MoveGen.cpp
@@ -10,24 +10,42 @@ TEST_CASE("Move generation", "[chess][movegen]") {
         REQUIRE(list.size() == 0);
     }
 
-    SECTION("(Invalid) Board with single piece has just one move") {
-        Color col = GENERATE(Color::White, Color::Black);
+    SECTION("Board with only colors not in turn has no moves") {
         Board board = Board::emptyBoard();
-        board.setPiece(4, 4, Piece(Piece::Type::Pawn, col));
+        Color c = opposite(board.colorToMove());
+        Piece p{GENERATE(Piece::Type::Pawn, Piece::Type::Rook, Piece::Type::Knight, Piece::Type::Bishop, Piece::Type::Queen, Piece::Type::King), c};
+        board.setPiece(4, 4, p);
         MoveList list = Chess::generateAllMoves(board);
-        REQUIRE(list.size() == 1);
+        REQUIRE(list.size() == 0);
     }
 
-    SECTION("(Invalid) Board with multiple pieces has multiple moves") {
-        uint8_t count = GENERATE(range(2u, 8u));
-        Color col1 = GENERATE(Color::White, Color::Black);
-        Color col2 = GENERATE(Color::White, Color::Black);
-        Board board = Board::emptyBoard();
-        for (uint8_t i = 0; i < count; i++) {
-            board.setPiece(i, 4, Piece(Piece::Type::Pawn, i & 1 ? col1 : col2));
+    SECTION("Invalid boards still generate moves") {
+
+        SECTION("Board with single pawn has just one move") {
+            Board board = Board::emptyBoard();
+            board.setPiece(4, 4, Piece(Piece::Type::Pawn, Color::White));
+            MoveList list = Chess::generateAllMoves(board);
+            REQUIRE(list.size() == 1);
         }
-        MoveList list = Chess::generateAllMoves(board);
-        REQUIRE(list.size() == count);
-    }
 
+        SECTION("Board with multiple pawns has multiple moves") {
+            uint8_t count = GENERATE(range(2u, 8u));
+            Board board = Board::emptyBoard();
+            for (uint8_t i = 0; i < count; i++) {
+                board.setPiece(i, 4, Piece(Piece::Type::Pawn, Color::White));
+            }
+            MoveList list = Chess::generateAllMoves(board);
+            REQUIRE(list.size() == count);
+        }
+
+        SECTION("Board with single rook always has 14 moves") {
+            uint8_t index = GENERATE(range(0u, 64u));
+            Board board = Board::emptyBoard();
+            for (uint8_t i = 0; i < index; i++) {
+                board.setPiece(index, Piece(Piece::Type::Rook, Color::White));
+            }
+            MoveList list = Chess::generateAllMoves(board);
+            REQUIRE(list.size() == index);
+        }
+    }
 }
