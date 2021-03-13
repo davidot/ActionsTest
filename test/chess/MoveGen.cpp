f9ba96e5be920715a014e8bcbe73f96b208386ca
davidot <david.tuin@gmail.com>
2021-03-13 16:42:20 +0100

    Extract color in move gen tests

--- test/chess/MoveGen.cpp
+++ test/chess/MoveGen.cpp
@@ -21,9 +21,11 @@ TEST_CASE("Move generation", "[chess][movegen]") {
 
     SECTION("Invalid boards still generate moves") {
 
+        Color toMove = Color::White;
+        [[maybe_unused]] Color other = opposite(Color::White); // TODO use or remove
         SECTION("Board with single pawn has just one move") {
             Board board = Board::emptyBoard();
-            board.setPiece(4, 4, Piece(Piece::Type::Pawn, Color::White));
+            board.setPiece(4, 4, Piece(Piece::Type::Pawn, toMove));
             MoveList list = Chess::generateAllMoves(board);
             REQUIRE(list.size() == 1);
         }
@@ -32,7 +34,7 @@ TEST_CASE("Move generation", "[chess][movegen]") {
             uint8_t count = GENERATE(range(2u, 8u));
             Board board = Board::emptyBoard();
             for (uint8_t i = 0; i < count; i++) {
-                board.setPiece(i, 4, Piece(Piece::Type::Pawn, Color::White));
+                board.setPiece(i, 4, Piece(Piece::Type::Pawn, toMove));
             }
             MoveList list = Chess::generateAllMoves(board);
             REQUIRE(list.size() == count);
@@ -41,7 +43,7 @@ TEST_CASE("Move generation", "[chess][movegen]") {
         SECTION("Board with single rook always has 14 moves") {
             uint8_t index = GENERATE(range(0u, 64u));
             Board board = Board::emptyBoard();
-            board.setPiece(index, Piece(Piece::Type::Rook, Color::White));
+            board.setPiece(index, Piece(Piece::Type::Rook, toMove));
             MoveList list = Chess::generateAllMoves(board);
             REQUIRE(list.size() == 14);
         }
@@ -50,7 +52,7 @@ TEST_CASE("Move generation", "[chess][movegen]") {
             uint8_t col = GENERATE(3, 4);
             uint8_t row = GENERATE(3, 4);
             Board board = Board::emptyBoard();
-            board.setPiece(col, row, Piece(Piece::Type::Bishop, Color::White));
+            board.setPiece(col, row, Piece(Piece::Type::Bishop, toMove));
             MoveList list = Chess::generateAllMoves(board);
             REQUIRE(list.size() == 13);
         }
@@ -59,23 +61,32 @@ TEST_CASE("Move generation", "[chess][movegen]") {
             uint8_t col = GENERATE(0, 7);
             uint8_t row = GENERATE(0, 7);
             Board board = Board::emptyBoard();
-            board.setPiece(col, row, Piece(Piece::Type::Bishop, Color::White));
+            board.setPiece(col, row, Piece(Piece::Type::Bishop, toMove));
             MoveList list = Chess::generateAllMoves(board);
             REQUIRE(list.size() == 7);
         }
 
         SECTION("King in the center has 8 moves") {
             Board board = Board::emptyBoard();
-            board.setPiece(4, 4, Piece(Piece::Type::King, Color::White));
+            board.setPiece(4, 4, Piece(Piece::Type::King, toMove));
             MoveList list = Chess::generateAllMoves(board);
             REQUIRE(list.size() == 8);
         }
 
         SECTION("Knight in the center has 8 moves") {
             Board board = Board::emptyBoard();
-            board.setPiece(4, 4, Piece(Piece::Type::Knight, Color::White));
+            board.setPiece(4, 4, Piece(Piece::Type::Knight, toMove));
             MoveList list = Chess::generateAllMoves(board);
             REQUIRE(list.size() == 8);
+        }
+
+        SECTION("Queen in the center has 14+13 moves") {
+            uint8_t col = GENERATE(3, 4);
+            uint8_t row = GENERATE(3, 4);
+            Board board = Board::emptyBoard();
+            board.setPiece(col, row, Piece(Piece::Type::Queen, toMove));
+            MoveList list = Chess::generateAllMoves(board);
+            REQUIRE(list.size() == 27);
         }
     }
 }
