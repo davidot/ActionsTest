615f17dc5d7d438157d851d3eef28c08f142bde5
davidot <david.tuin@gmail.com>
2021-03-16 01:28:12 +0100

    Remove coverage comment from verify since it does not work anyway

--- src/util/Assertions.h
+++ src/util/Assertions.h
@@ -2,17 +2,13 @@
 
 #ifndef NDEBUG
 #include <iostream>
-#define BASE_VERIFY(expr) \
-    do {                  \
-        /*GCOVR_EXCL_START*/ \
-        if (!static_cast<bool>(expr)) { \
+#define VERIFY(expr)                                                                          \
+    do {                                                                                      \
+        if (!static_cast<bool>(expr)) {                                                       \
             std::cout << "VERIFY: " << #expr << " failed in " << __FILE__ << ":" << __LINE__; \
-            std::exit(-3); \
-        } \
-    } while (0)           \
-    /*GCOVR_EXCL_STOP*/
-
-#define VERIFY(expr) BASE_VERIFY(expr)
+            std::exit(-3);                                                                    \
+        }                                                                                     \
+    } while (0)
 
 #define VERIFY_NOT_REACHED() VERIFY(false)
 #endif
