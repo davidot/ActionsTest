89d6cc3680f11b60293aad8d688ffb6765c7cce7
davidot <david.tuin@gmail.com>
2021-03-20 01:35:34 +0100

    Remove pretty func since MSVC does not have it

--- src/util/Assertions.cpp
+++ src/util/Assertions.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 namespace util::Assert {
-    void assertFailed(const char *assertion, const char *file, int line, const char *func) {
+    void assertFailed(const char *assertion, const char *file, int line) {
         std::cerr << "Assertion: " << assertion << " failed! " << file << ':' << line << " (in " << func << ")\n";
 #if defined __has_builtin
 #  if __has_builtin (__builtin_trap)
--- src/util/Assertions.h
+++ src/util/Assertions.h
@@ -1,14 +1,14 @@
 #pragma once
 
 namespace util::Assert {
-    void assertFailed(const char *assertion, const char *file, int line, const char *func);
+    void assertFailed(const char *assertion, const char *file, int line);
 }
 
 #ifndef NDEBUG
 #define ASSERT(expr)                                         \
     do {                                                     \
         if (!static_cast<bool>(expr)) {                      \
-            util::Assert::assertFailed(#expr, __FILE__, __LINE__, __PRETTY_FUNCTION__); \
+            util::Assert::assertFailed(#expr, __FILE__, __LINE__); \
         }                                                    \
     } while (0)
 
