116ce7cc063546127f708510d174134fbe6ba967
davidot <david.tuin@gmail.com>
2021-03-23 14:42:42 +0100

    Use sampling instead of first n with small tests
    This should increase the parts we are actually testing instead of always only picking the first value

--- test/chess/TestUtil.h
+++ test/chess/TestUtil.h
@@ -1,7 +1,68 @@
 #pragma once
 
+#include <catch2/catch.hpp>
+#include <deque>
 #ifndef EXTENDED_TESTS
-#define TEST_SOME(x) take(2, x)
+#define TEST_SOME(x) sample(2, x)
 #else
 #define TEST_SOME(x) x
 #endif
+
+
+namespace Catch::Generators {
+
+    template<typename T>
+    class SamplingGenerator final : public IGenerator<T> {
+        std::vector<T> m_values;
+
+    public:
+        SamplingGenerator(size_t target, GeneratorWrapper<T> &&generator) : SamplingGenerator(target, std::numeric_limits<size_t>::max(), std::move(generator)) {};
+
+        SamplingGenerator(size_t target, size_t generatorSize, GeneratorWrapper<T> &&generator) {
+            assert(target != 0 && "Empty generators are not allowed");
+            size_t generated = 0;
+            m_values.reserve(target);
+            for (; generated < target; ++generated) {
+                m_values.push_back(generator.get());
+                if (!generator.next()) {
+                    // ran out of values already
+                    return;
+                }
+            }
+
+            Catch::SimplePcg32& random = rng();
+            do {
+                auto j = std::uniform_int_distribution<size_t>(0, generated)(random);
+                if (j < target) {
+                    m_values[j] = generator.get();
+                }
+                ++generated;
+            } while (generated < generatorSize && generator.next());
+            if (generated == generatorSize && generatorSize >= 1000 * target) {
+                Catch::throw_exception(GeneratorException("You probably want to limit the sample size"));
+            }
+        }
+
+        T const &get() const override {
+            return m_values.back();
+        }
+
+        bool next() override {
+            if (!m_values.empty()) {
+                m_values.pop_back();
+            }
+            return !m_values.empty();
+        }
+    };
+
+    template<typename T>
+    GeneratorWrapper<T> sample(size_t target, GeneratorWrapper<T> &&generator) {
+        return GeneratorWrapper<T>(pf::make_unique<SamplingGenerator<T>>(target, std::move(generator)));
+    }
+
+    template<typename T>
+    GeneratorWrapper<T> sample(size_t target, size_t totalSize, GeneratorWrapper<T> &&generator) {
+        return GeneratorWrapper<T>(pf::make_unique<SamplingGenerator<T>>(target, totalSize, std::move(generator)));
+    }
+
+}// namespace Catch::Generators
