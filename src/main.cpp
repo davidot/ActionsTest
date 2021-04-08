b86908fa40f09f7fb78eca6c76803e547852a5ba
davidot <david.tuin@gmail.com>
2021-04-08 11:56:48 +0200

    Add move count output to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <chess/Board.h>
+#include <chess/MoveGen.h>
 #include <iostream>
 
 int main() {
     Chess::Board board = Chess::Board::standardBoard();
-    std::cout << "Standard fen: " << board.toFEN() << '\n';
+
+    std::cout << "Start fen: " << board.toFEN() << '\n';
+
+    Chess::MoveList list = Chess::generateAllMoves(board);
+
+    std::cout << "Found " << list.size() << " moves from start position\n";
+
     return 0;
 }
