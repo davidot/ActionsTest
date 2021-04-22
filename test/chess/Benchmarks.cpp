ecfcdf17adb44e594864933828753369ecbbf698
davidot <david.tuin@gmail.com>
2021-04-23 00:19:25 +0200

    Add SAN move parsing benchmarks
    In the future we might want to implement with isLegal or something

--- test/chess/Benchmarks.cpp
+++ test/chess/Benchmarks.cpp
@@ -96,6 +96,46 @@ TEST_CASE("MoveGen benchmarks", "[movegen]" BENCHMARK_TAGS) {
 }
 #undef TEST_FEN
 
+TEST_CASE("SAN moves benchmarks", "[san]" BENCHMARK_TAGS) {
+#define TEST_FEN(fen, note)                                          \
+    {                                                                \
+        auto eb = Board::fromFEN(fen);                               \
+        REQUIRE(eb);                                                 \
+        Board b = eb.extract();                                      \
+                                                                     \
+        BENCHMARK("PGN from FEN " note) {                     \
+            auto moves = generateAllMoves(b);                        \
+            int i = 0;                                               \
+            moves.forEachMove([&](const Move& move) {                \
+                pgns[i] = b.moveToSAN(move, moves);                  \
+                ++i;                                                 \
+                REQUIRE(move == b.parseSANMove(pgns[i - 1], moves)); \
+            });                                                      \
+        };                                                           \
+    }
+
+    std::array<std::string, 256> pgns;
+
+    TEST_FEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "Start pos");
+
+    TEST_FEN("R6R/3Q4/1Q4Q1/4Q3/2Q4Q/Q4Q2/pp1Q4/kBNN1KB1 w - - 0 1", "Many moves 1");
+    TEST_FEN("3Q4/1Q4Q1/4Q3/2Q4R/Q4Q2/3Q4/1Q4Rp/1K1BBNNk w - - 0 1", "Many moves 2");
+    TEST_FEN("8/P1Q4P/6R1/1K1B1N2/3B4/P2N3P/8/1k6 w - - 0 1", "Many moves 3");
+    TEST_FEN("8/P4K1P/k1B1N3/4BN1P/1Q6/3R4/P1P1PP1P/6R1 w - - 0 1", "Many moves 4");
+
+    TEST_FEN("4Q2Q/4r3/6n1/1bbK1krn/RR1RRnRR/2qn1R1n/4n1nN/Q3Q3 w - - 1 2", "In check");
+
+    TEST_FEN("k7/2QQQQQQ/KQ1QQQQQ/QQQ1QQQQ/QQQQ1QQQ/QQQQQ1QQ/QQQQQQ1Q/QQQQQQQ1 w - - 0 1", "Lots of queens");
+
+    TEST_FEN("kqQQqQqq/qq5Q/Q6q/q6Q/Q6q/Q6q/Q5QQ/qQqqQqQK w - - 0 1", "Many queens both W");
+    TEST_FEN("kqQQqQqq/qq5Q/Q6q/q6Q/Q6q/Q6q/Q5QQ/qQqqQqQK b - - 0 1", "Many queens both B");
+
+    TEST_FEN("1n1r1b1r/P1P1P1P1/2BNq1k1/7R/3Q4/1P1N2K1/P1PBP3/5R2 w - - 15 45", "Legal pos many moves");
+
+    TEST_FEN("RNBQKBNR/PPPPPPPP/8/8/8/8/pppppppp/rnbqkbnr w - - 0 1", "Inverted start pos");
+}
+#undef TEST_FEN
+
 template<bool output = false>
 uint64_t countMoves(Board& board, int depth) {
     if (depth <= 0) {
