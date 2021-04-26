96c8189dc6d7a3b0d6392b450233cf55808a9828
davidot <david.tuin@gmail.com>
2021-04-26 17:43:29 +0200

    Add convenience functions to load and generate seeds

---
+++ src/util/RandomUtil.cpp
@@ -0,0 +1,71 @@
+#include "RandomUtil.h"
+#include "Assertions.h"
+#include <functional>
+#include <iomanip>
+#include <sstream>
+
+namespace util {
+
+    using seed_int = std::random_device::result_type;
+    constexpr size_t hex_per_int = sizeof(seed_int) * 2;
+    constexpr size_t int_per_row = 8;
+
+    std::string outputSeed(const std::vector<uint32_t>& values) {
+        std::ostringstream stream;
+        stream << std::hex << std::setfill('0');
+
+        constexpr const char* spacing = "";
+
+        stream << spacing;
+
+        for (size_t i = 0; i < values.size(); i++) {
+            stream << std::setw(hex_per_int) << values[i];
+
+            if ((i + 1) >= values.size()) {
+                break;
+            }
+
+            if (i % int_per_row == int_per_row - 1) {
+                // was last in row
+                stream << '\n' << spacing;
+            } else {
+                stream << ' ';
+            }
+        }
+
+        return stream.str();
+    }
+
+
+    std::pair<seed_vector, std::string> generateSeed(size_t bytes) {
+        ASSERT(bytes % sizeof(seed_int) == 0);
+        const size_t ints = bytes / sizeof(seed_int);
+        std::vector<uint32_t> values(ints);
+        {
+            std::random_device randomDevice;
+            for (size_t i = 0; i < ints; i++) {
+                values[i] = randomDevice();
+            }
+        }
+
+        return {values, outputSeed(values)};
+    }
+
+    std::optional<seed_vector> loadSeed(const std::string& mnemonic, size_t bytes) {
+        ASSERT(bytes % sizeof(seed_int) == 0);
+        const size_t ints = bytes / sizeof(seed_int);
+        std::istringstream inputString(mnemonic);
+        inputString >> std::skipws >> std::hex;
+
+        std::vector<uint32_t> values(ints);
+
+        for (size_t i = 0; i < ints; i++) {
+            inputString >> std::setw(hex_per_int) >> values[i];
+            if (!inputString) {
+                return std::nullopt;
+            }
+        }
+
+        return std::move(values);
+    }
+}
--- src/util/RandomUtil.h
+++ src/util/RandomUtil.h
@@ -1,19 +1,50 @@
 #pragma once
 
+#include <optional>
+#include <tuple>
 #include <random>
+
 namespace util {
 
+    using seed_vector = std::vector<std::random_device::result_type>;
+
+    std::pair<seed_vector, std::string> generateSeed(size_t bytes);
+
+    std::optional<seed_vector> loadSeed(const std::string& mnemonic, size_t bytes);
+
     template<typename RNG>
-    RNG seedRNG() {
-        std::random_device source;
+    RNG seedRNG(std::string* textOutput = nullptr, size_t bytes = RNG::state_size * sizeof(RNG::word_size)) {
+        auto [seed, mnemonic] = generateSeed(bytes);
 
-        std::vector<std::random_device::result_type> bits(RNG::word_size);
-        std::generate(std::begin(bits), std::end(bits), [&](){return source();});
+        if (textOutput != nullptr) {
+            *textOutput = std::move(mnemonic);
+        }
 
-        std::seed_seq seeds(std::begin(bits), std::end(bits));
-        RNG rng (seeds);
-        return rng;
+        std::seed_seq seq(seed.begin(), seed.end());
+        return RNG(seq);
     }
+
+    template<typename RNG>
+    std::optional<RNG> seedRNGFromString(std::string& textOutput, size_t bytes = RNG::state_size * sizeof(RNG::word_size)) {
+        seed_vector seed;
+
+        if (textOutput.empty()) {
+            auto [seedVal, mnemonic] = generateSeed(bytes);
+            textOutput = std::move(mnemonic);
+            seed = std::move(seedVal);
+        } else {
+            auto opt = loadSeed(textOutput, bytes);
+            if (!opt) {
+                return std::nullopt;
+            }
+            seed = opt.value();
+        }
+
+        std::seed_seq seq(seed.begin(), seed.end());
+        return RNG(seq);
+    }
+
+
 
 }
 
