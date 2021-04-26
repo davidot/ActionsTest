31e0df5cc1f7425d0cbb4a0061ae3a8798baabf8
davidot <david.tuin@gmail.com>
2021-04-26 15:20:09 +0200

    Add repetition tests

--- test/chess/Repetition.cpp
+++ test/chess/Repetition.cpp
@@ -1,8 +1,9 @@
 #include "TestUtil.h"
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators_range.hpp>
 #include <chess/Board.h>
 
-TEST_CASE("Can detect repetition") {
+TEST_CASE("Can detect repetition", "[chess][legal]") {
 
     using namespace Chess;
 
@@ -18,46 +19,90 @@ TEST_CASE("Can detect repetition") {
         Move blackMove2{"h6", "g8"};
 
 // Any of the boards should all have occurred repeated times in history
-#define REPEAT_MOVE(times) \
-    do {                   \
-       auto repeated = board.positionRepeated(); \
-    for (int i = repeated; i < repeated + (times); ++i) {\
-        CAPTURE(i);\
-        REQUIRE(board.positionRepeated() == i);\
-        board.makeMove(whiteMove1);\
-        REQUIRE(board.positionRepeated() == i);\
-        board.makeMove(whiteMove2);\
-        REQUIRE(board.positionRepeated() == i);\
-        board.makeMove(blackMove1);\
-        REQUIRE(board.positionRepeated() == i);\
-        board.makeMove(blackMove2);\
-        REQUIRE(board.positionRepeated() == i + 1);\
-    } \
-    } while(false)
+#define REPEAT_MOVE(times)                   \
+    do {                                     \
+        for (auto i = 0; i < (times); ++i) { \
+            board.makeMove(whiteMove1);      \
+            board.makeMove(blackMove1);      \
+            board.makeMove(whiteMove2);      \
+            board.makeMove(blackMove2);      \
+        }                                    \
+    } while (false)
 
-        SECTION("Same knight moves") {
-            REPEAT_MOVE(10);
-            REQUIRE(board.positionRepeated() == 10);
+        SECTION("Single repetition") {
+            REQUIRE(board.positionRepeated() == 0);
+            board.makeMove(whiteMove1);
+            REQUIRE(board.positionRepeated() == 0);
+            board.makeMove(blackMove1);
+            REQUIRE(board.positionRepeated() == 0);
+            board.makeMove(whiteMove2);
+            REQUIRE(board.positionRepeated() == 0);
+            board.makeMove(blackMove2);
+            REQUIRE(board.positionRepeated() == 1);
         }
 
-        SECTION("Pawn move breaks repetition") {
-            Move mv{"e2", "e4", Move::Flag::DoublePushPawn};
-            Move mv2{"e7", "e5", Move::Flag::DoublePushPawn};
+        SECTION("Same knight moves") {
+            // test some generate range
+            size_t times = GENERATE(TEST_SOME(range(1, 11)));
+            CAPTURE(times);
+            REPEAT_MOVE(times);
+            REQUIRE(board.positionRepeated() == times);
+            for (int i = 0; i < 3; i++) {
+                CAPTURE(i);
+                board.undoMove();
+                REQUIRE(board.positionRepeated() == times - 1);
+            }
+        }
 
-            REPEAT_MOVE(2);
-
-            REQUIRE(board.positionRepeated() == 2);
-
-
-            board.makeMove(mv);
+        SECTION("Position with en passant differs from no en passant") {
+            board.makeMove({"e2", "e4", Move::Flag::DoublePushPawn});
             REQUIRE(board.positionRepeated() == 0);
-            board.makeMove(mv2);
+
             REQUIRE(board.positionRepeated() == 0);
-            REQUIRE(board.enPassantColRow().has_value());
+            board.makeMove(blackMove1);
+            REQUIRE(board.positionRepeated() == 0);
+            board.makeMove(whiteMove1);
+            REQUIRE(board.positionRepeated() == 0);
+            board.makeMove(blackMove2);
+            REQUIRE(board.positionRepeated() == 0);
+            board.makeMove(whiteMove2);
+            REQUIRE(board.positionRepeated() == 0);
+        }
 
+        SECTION("CastlingRights change breaks repetition") {
+            board.setPiece(Board::queenSideRookCol, Board::pawnHomeRow(Color::White), std::nullopt);
 
-            REPEAT_MOVE(2);
-            REQUIRE(board.positionRepeated() == 2);
+            REQUIRE(board.positionRepeated() == 0);
+            board.makeMove({Board::queenSideRookCol, Board::homeRow(Color::White), Board::queenSideRookCol, Board::pawnHomeRow(Color::White)});
+            REQUIRE(board.positionRepeated() == 0);
+            CHECK(board.castlingRights() != CastlingRight::AnyCastling);
+
+            board.makeMove(blackMove1);
+            REQUIRE(board.positionRepeated() == 0);
+
+            board.makeMove({Board::queenSideRookCol, Board::pawnHomeRow(Color::White), Board::queenSideRookCol, Board::homeRow(Color::White)});
+            REQUIRE(board.positionRepeated() == 0);
+
+            board.makeMove(blackMove2);
+            // 0 since castling rights are different!
+            REQUIRE(board.positionRepeated() == 0);
+        }
+
+        SECTION("Repetition holds even with moves in between") {
+            REPEAT_MOVE(3);
+            REQUIRE(board.positionRepeated() == 3);
+
+            board.makeMove({"g1", "h3"});
+            REQUIRE(board.positionRepeated() == 0);
+            board.makeMove({"b8", "a6"});
+            REQUIRE(board.positionRepeated() == 0);
+            board.makeMove({"h3", "g1"});
+            REQUIRE(board.positionRepeated() == 0);
+            board.makeMove({"a6", "b8"});
+            REQUIRE(board.positionRepeated() == 4);
+
+            REPEAT_MOVE(3);
+            REQUIRE(board.positionRepeated() == 7);
         }
     }
 
