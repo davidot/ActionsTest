ecf81bdcbd9d2dbb674907aaa7eca728d07620ef
davidot <david.tuin@gmail.com>
2021-04-30 23:48:48 +0200

    Add arrows for moves and last moves

--- visual/main.cpp
+++ visual/main.cpp
@@ -41,7 +41,15 @@ int main() {
     io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
     Chess::Board board = Chess::Board::standardBoard();
+    Chess::Move lastMove{};
     sf::Vector2f boardOffset = {100, 100};
+
+    sf::Text edit;
+    edit.setFont(font);
+    edit.setCharacterSize(24);
+    edit.setString("Edit mode enabled");
+    edit.setFillColor(sf::Color::Green);
+    edit.setPosition(boardOffset);
 
     sf::Text text;
     text.setFont(font); // font is a sf::Font
@@ -58,21 +66,78 @@ int main() {
     highlightSquare.setOutlineThickness(2.5);
     highlightSquare.setFillColor(sf::Color::Transparent);
 
-
+    bool editMode = false;
     sf::Vector2i selectedSquare = {-1, -1};
 
-    constexpr const int boardSquareSize = 8;
+    constexpr const float boardSquareSize = 8;
     auto colRowToRect = [&](uint8_t col, uint8_t row) {
       return boardOffset + sf::Vector2f {squareSize * col, squareSize * (boardSquareSize - row)};
     };
 
-    std::string fenStatus = "";
+    auto drawArrow = [&](int colFrom, int rowFrom, int colTo, int rowTo,
+                         sf::Color bottomCol = {0x59, 0x98, 0x1A},
+                         sf::Color topCol = {0x81, 0xB6, 0x22}) {
+        if (colFrom == colTo && rowFrom == rowTo) {
+            return;
+        }
+        auto centerBack = colRowToRect(colFrom, rowFrom) + sf::Vector2f{squareSize / 2.0, squareSize / 2.0};
+
+        auto centerFront = colRowToRect(colTo, rowTo) + sf::Vector2f{squareSize / 2.0, squareSize / 2.0};
+//
+//        sf::VertexArray li{sf::Lines};
+//        li.append(centerBack);
+//        li.append(centerFront);
+//        li[1].color = sf::Color::Blue;
+
+        auto line = centerFront - centerBack;
+        line *= 1.0f / std::sqrt(line.x * line.x + line.y * line.y);
+
+        float thickness = 6.f;
+        float relArrowSize = 4.f;
+
+        auto lineHead = centerFront - (relArrowSize * thickness * line);
+
+        sf::Vector2f normal1{-line.y, line.x};
+        sf::Vector2f normal2{line.y, -line.x};
+
+        sf::VertexArray v{sf::Triangles};
+
+        v.append(centerBack + (normal1 * thickness));
+        v.append(centerBack + (normal2 * thickness));
+        v.append(lineHead + (normal2 * thickness));
+
+        v.append(centerBack + (normal1 * thickness));
+        v.append(lineHead + (normal1 * thickness));
+        v.append(lineHead + (normal2 * thickness));
+
+        v.append(lineHead + (normal1 * thickness * relArrowSize));
+        v.append(centerFront);
+        v.append(lineHead + (normal2 * thickness * relArrowSize));
+
+        for (size_t i = 0; i < v.getVertexCount(); i++) {
+            sf::Color& c = (i < 2u || i == 3) ? bottomCol : topCol;
+            v[i].color = c;
+        }
+
+        window.draw(v);
+//
+//        sf::VertexArray li2{sf::Lines};
+//        li2.append(centerBack);
+//        li2.append(centerBack + (normal1 * thickness));
+//        li2[0].color = sf::Color::Red;
+//        li2[1].color = sf::Color::Red;
+//
+//        window.draw(li);
+//        window.draw(li2);
+    };
+
+    std::string fenStatus;
 
     auto rng = util::seedRNG<std::mt19937_64>();
 
     sf::Clock deltaClock;
     while (window.isOpen()) {
-        sf::Event event;
+        sf::Event event{};
         bool clicked = false;
         while (window.pollEvent(event)) {
             ImGui::SFML::ProcessEvent(event);
@@ -87,8 +152,17 @@ int main() {
                 clicked = true;
             }
 
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space) {
-                board.makeNullMove();
+            if (event.type == sf::Event::KeyPressed){
+                switch (event.key.code) {
+                    case sf::Keyboard::Space:
+                        board.makeNullMove();
+                        break;
+                    case sf::Keyboard::E:
+                        editMode = !editMode;
+                        break;
+                    default:
+                        break;
+                }
             }
         }
 
@@ -112,6 +186,8 @@ int main() {
                 fenStatus = "Copied!";
             }
             ImGui::SameLine(0.0f, 5.0f);
+
+            bool boardChanged = false;
             if (ImGui::Button("Paste string value")) {
                 auto s = sf::Clipboard::getString().toAnsiString();
                 auto eBoard = Chess::Board::fromFEN(s);
@@ -119,14 +195,20 @@ int main() {
                     fenStatus = "Invalid FEN: _" + s + "_\n Got: " + eBoard.error();
                 } else {
                     board = eBoard.extract();
+                    boardChanged = true;
                     fenStatus = "Loaded";
                 }
             }
             if (ImGui::Button("Reset board")) {
                 board = Chess::Board::standardBoard();
+                boardChanged = true;
             }
 
             ImGui::Text("%s", fenStatus.c_str());
+
+            if (boardChanged) {
+                list = Chess::generateAllMoves(board);
+            }
 
             ImGui::Separator();
             ImGui::Text("%zu moves!", list.size());
@@ -193,6 +275,7 @@ int main() {
                 });
 
                 if (selected.fromPosition != selected.toPosition) {
+                    lastMove = selected;
                     board.makeMove(selected);
                 }
             } else {
@@ -240,6 +323,9 @@ int main() {
         mousePosition -= sf::Vector2i(boardOffset.x, boardOffset.y);
         mousePosition /= (int)squareSize;
         mousePosition = sf::Vector2i(mousePosition.x, boardSquareSize - mousePosition.y);
+
+        std::optional<sf::Vector2i> moveTo;
+
         if (mousePosition.x >= 0 && mousePosition.x < boardSquareSize
            && mousePosition.y >= 0 && mousePosition.y < boardSquareSize) {
 
@@ -249,12 +335,16 @@ int main() {
             highlightSquare.setOutlineColor(sf::Color(0, 0, 255, 200));
             window.draw(highlightSquare);
             if (clicked) {
-                sf::Vector2i newPosition {mousePosition.x, mousePosition.y};
+                sf::Vector2i newPosition{mousePosition.x, mousePosition.y};
                 if (selectedSquare.x >= 0) {
-                    auto pieceFrom = board.pieceAt(selectedSquare.x, selectedSquare.y);
-                    board.setPiece(selectedSquare.x, selectedSquare.y, std::nullopt);
-                    board.setPiece(newPosition.x, newPosition.y, pieceFrom);
-                    selectedSquare = {-1, -1};
+                    if (editMode) {
+                        auto pieceFrom = board.pieceAt(selectedSquare.x, selectedSquare.y);
+                        board.setPiece(selectedSquare.x, selectedSquare.y, std::nullopt);
+                        board.setPiece(newPosition.x, newPosition.y, pieceFrom);
+                        selectedSquare = newPosition;
+                    } else {
+                        moveTo = newPosition;
+                    }
                 } else {
                     selectedSquare = newPosition;
                 }
@@ -264,14 +354,22 @@ int main() {
         }
 
         if (selectedSquare.x >= 0) {
+            drawArrow(selectedSquare.x, selectedSquare.y, mousePosition.x, mousePosition.y, sf::Color::Blue, sf::Color::Red);
+
             sf::Vector2f position = colRowToRect(selectedSquare.x, selectedSquare.y);
             highlightSquare.setPosition(position);
 
             highlightSquare.setOutlineColor(sf::Color(255, 0, 0, 200));
             window.draw(highlightSquare);
 
+            Chess::Move mv;
+
             list.forEachMoveFrom(selectedSquare.x, selectedSquare.y, [&](const Chess::Move& move) {
                 auto [col, row] = move.colRowToPosition();
+                if (moveTo.has_value() && col == moveTo->x && row == moveTo->y) {
+                    mv = move;
+                }
+
                 if (move.flag == Chess::Move::Flag::None) {
                     highlightSquare.setOutlineColor(sf::Color(0, 255, 0, 200));
                 } else if (move.flag == Chess::Move::Flag::Castling) {
@@ -285,10 +383,33 @@ int main() {
                 highlightSquare.setPosition(position);
                 window.draw(highlightSquare);
             });
+
+            if (moveTo.has_value() && mv.fromPosition != mv.toPosition) {
+                lastMove = mv;
+                board.makeMove(mv);
+            }
+
         }
+
+        if (moveTo.has_value()) {
+            selectedSquare = {-1, -1};
+        }
+
+        if (lastMove.fromPosition != lastMove.toPosition) {
+            static sf::Color lastColor {184, 15, 10, 100};
+            auto [colFrom, rowFrom] = lastMove.colRowFromPosition();
+            auto [colTo, rowTo] = lastMove.colRowToPosition();
+            drawArrow(colFrom, rowFrom, colTo, rowTo, lastColor, lastColor);
+        }
+
+//        drawArrow(0, 0, mousePosition.x, mousePosition.y);
 
         text.setString(std::to_string(mousePosition.x) + "," + std::to_string(mousePosition.y) + (board.colorToMove() == Chess::Color::White ? " W" : " B"));
         window.draw(text);
+        if (editMode) {
+            window.draw(edit);
+        }
+
 
 
         ImGui::SFML::Render(window);
