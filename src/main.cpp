da3675c9494a7264c942c50554d287de31e4a514
davidot <david.tuin@gmail.com>
2021-05-18 15:59:13 +0200

    Remove now duplicate code in main to play game

--- src/main.cpp
+++ src/main.cpp
@@ -15,181 +15,45 @@
 int main(int argc, char** argv) {
     int status = 0;
 
-    auto rand = Chess::randomPlayer();
+//    auto rand = Chess::randomPlayer();
 
-    auto players = {
-            Chess::indexPlayer(0),
-            Chess::indexPlayer(1),
-            Chess::indexPlayer(8),
-            Chess::indexPlayer(-1),
-            Chess::indexPlayer(-4),
-    };
-
-    for (const auto& white : players) {
-        for (const auto& black : players) {
-            auto res = Chess::playGame(white, black);
-            if (res.final() != Chess::GameResult::Final::Draw) {
-                std::cout << "Non draw! "
-                          << (res.final() == Chess::GameResult::Final::BlackWin ? "Black" : "White")
-                          << " won\n"
-                          << white->name() << " vs " << black->name() << "\n"
-                          << "PGN: " << res.pgn << '\n';
-            }
-        }
-    }
+//    auto players = {
+//            Chess::indexPlayer(0),
+//            Chess::indexPlayer(1),
+//            Chess::indexPlayer(8),
+//            Chess::indexPlayer(-1),
+//            Chess::indexPlayer(-4),
+//    };
+//
+//    for (const auto& white : players) {
+//        for (const auto& black : players) {
+//            auto res = Chess::playGame(white, black);
+//            if (res.final() != Chess::GameResult::Final::Draw) {
+//                std::cout << "Non draw! "
+//                          << (res.final() == Chess::GameResult::Final::BlackWin ? "Black" : "White")
+//                          << " won\n"
+//                          << white->name() << " vs " << black->name() << "\n"
+//                          << "PGN: " << res.pgn << '\n';
+//            }
+//        }
+//    }
 
     auto minOpp = Chess::minOpponentMoves();
+    auto maxOpp = Chess::maxOpponentMoves();
 
-    auto oppRes = Chess::playGame(minOpp, minOpp);
-    std::cout << "Non draw! "
-              << oppRes.stringifyResult()
-              << " won\n"
-              << minOpp->name() << " vs " << minOpp->name() << "\n"
-              << "PGN: " << oppRes.pgn << '\n';
 
-    Chess::Board board = Chess::Board::standardBoard();
+    auto playGame = [](auto& white, auto& black) {
+        auto result = Chess::playGame(white, black);
+        std::cout << white->name() << " vs " << black->name() << "\n"
+                  << result.stringifyResult() << '\n'
+                  << "PGN: " << result.pgn << "\n\n";
+    };
 
-    Chess::MoveList moves = Chess::generateAllMoves(board);
 
-    std::string seed;
-    auto orng = util::seedRNGFromString<std::mt19937_64>(seed, 64);
-    if (!orng.has_value()) {
-        std::cout << "Wrong seed\n";
-        return 2;
-    }
-    auto rng = orng.value();
-
-    std::cout << "Using seed:\n" << seed << '\n';
-
-    size_t moveNum = 0;
-    std::ostringstream pgn;
-
-    std::optional<Chess::Color> capturer;
-    bool both = false;
-
-    if (argc > 1) {
-        char first = argv[1][0];
-        capturer = (first == 'w' || first == 'W') ? Chess::Color::White : Chess::Color::Black;
-        if (argv[1] == std::string("wb")) {
-            both = true;
-            std::cout << "Using capturer: both\n";
-        } else {
-            std::cout << "Using capturer: " << capturer.value() << '\n';
-        }
-    }
-
-    while (moves.size() > 0 && moveNum < 1000) {
-        size_t moveIndex = std::uniform_int_distribution<size_t>(0, moves.size() - 1)(rng);
-
-        Chess::Move mv;
-
-        if (board.colorToMove() == capturer || both) {
-            // prefer captures
-            int count = 0;
-            moves.forEachMove([&](const Chess::Move& move){
-                auto [toCol, toRow] = move.colRowToPosition();
-                if (board.pieceAt(toCol, toRow) != std::nullopt) {
-                    count++;
-                }
-            });
-
-            if (count > 0) {
-                moveIndex = std::uniform_int_distribution<size_t>(0, count - 1)(rng);
-                moves.forEachFilteredMove([&](const Chess::Move& move){
-                  auto [toCol, toRow] = move.colRowToPosition();
-                  return board.pieceAt(toCol, toRow) != std::nullopt;
-                }, [&mv, moveIndex, i = 0u](const Chess::Move& move) mutable {
-                  if (i++ == moveIndex) {
-                      mv = move;
-                  }
-                });
-            }
-        }
-
-        if (mv.fromPosition == mv.toPosition) {
-            moves.forEachMove([&mv, moveIndex, i = 0u](const Chess::Move& move) mutable {
-              if (i++ == moveIndex) {
-                  mv = move;
-              }
-            });
-        }
-
-        if (board.colorToMove() == Chess::Color::White) {
-            pgn << board.fullMoves() << ". ";
-        }
-
-        pgn << board.moveToSAN(mv) << " ";
-
-        board.makeMove(mv);
-
-        moveNum++;
-        moves = Chess::generateAllMoves(board);
-
-        if (moves.size() > 0 && board.isDrawn()) {
-            if (board.halfMovesSinceIrreversible() > 99 && board.positionRepeated() > 2) {
-                std::cout << "Draw by no irreversible halfmove !And! repetition\n";
-            } else if (board.halfMovesSinceIrreversible() > 99) {
-                std::cout << "Draw by no irreversible halfmove for 100 moves\n";
-            } else if (board.positionRepeated() > 2) {
-                std::cout << "Draw by a 3 times repeated position\n";
-            } else {
-                std::cout << "Draw by ????\n";
-                status = 4;
-            }
-            break;
-        }
-
-        if (board.countPieces(Chess::Color::White) == 1 && board.countPieces(Chess::Color::Black) == 1) {
-            std::cout << "Draw by just kings left (boring)\n";
-
-            bool white = false;
-            bool black = false;
-
-            for (int col = 0; col < 8; col++) {
-                for (int row = 0; row < 8; row++) {
-                    auto p = board.pieceAt(col, row);
-                    if (p.has_value()) {
-                        if (p->type() != Chess::Piece::Type::King) {
-                            std::cout << "Non king at" << col << ',' << row << " ??\n";
-                            status = 5;
-                        } else {
-                            if (p->color() == Chess::Color::Black) {
-                                if (black) {
-                                    std::cout << "Two black kings??? at" << col << ',' << row << " ??\n";
-                                    status = 6;
-                                }
-                                black = true;
-                            } else {
-                                if (white) {
-                                    std::cout << "Two white kings??? at" << col << ',' << row << " ??\n";
-                                    status = 6;
-                                }
-                                white = true;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!white || !black) {
-                std::cout << "No king(s) left black: " << black << " white: " << white << " ??\n";
-                status = 7;
-            }
-
-            break;
-        }
-
-    }
-
-    if (moves.isCheckMate()) {
-        std::cout << "Checkmate!\n";
-    } else if (moves.isStaleMate()) {
-        std::cout << "Stalemate!\n";
-    }
-
-    std::cout << "Finished game!\n" << board.toFEN() << " after " << moveNum << " moves\n";
-
-    std::cout << pgn.str();
+//    playGame(minOpp, minOpp);
+//    playGame(maxOpp, minOpp);
+    playGame(minOpp, maxOpp);
+//    playGame(maxOpp, maxOpp);
 
     return status;
 }
