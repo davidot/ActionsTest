8f8116487aafa37e79b937f6d0198db40b978623
davidot <david.tuin@gmail.com>
2021-05-22 01:42:13 +0200

    Add new players to main

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,13 @@ int main(int argc, char** argv) {
 
     auto minOpp = Chess::minOpponentMoves();
     auto maxOpp = Chess::maxOpponentMoves();
+    auto lexi1 = Chess::lexicographically(true, true);
+    auto lexi2 = Chess::lexicographically(false, true);
+    auto lexi3 = Chess::lexicographically(true, false);
+    auto lexi4 = Chess::lexicographically(false, false);
+    auto alpha = Chess::alphabetically(true);
+    auto alphaR = Chess::alphabetically(false);
+    auto rand = Chess::randomPlayer();
 
 
     auto playGame = [](auto& white, auto& black) {
@@ -47,13 +54,22 @@ int main(int argc, char** argv) {
         std::cout << white->name() << " vs " << black->name() << "\n"
                   << result.stringifyResult() << '\n'
                   << "PGN: " << result.pgn << "\n\n";
+        return result;
     };
 
 
 //    playGame(minOpp, minOpp);
 //    playGame(maxOpp, minOpp);
-    playGame(minOpp, maxOpp);
-//    playGame(maxOpp, maxOpp);
+//    playGame(minOpp, maxOpp);
+    playGame(maxOpp, maxOpp);
+
+    std::vector<std::reference_wrapper<std::unique_ptr<Chess::Player>>> players = { lexi1, lexi2, lexi3, lexi4, alpha, alphaR};
+
+//    for (auto& w : players) {
+//        for (auto& b : players) {
+//            playGame(w.get(), b.get());
+//        }
+//    }
 
     return status;
 }
